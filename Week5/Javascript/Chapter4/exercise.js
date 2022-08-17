82551f1b146e2775408049dca7c4e6af5f3d63bd
@EdwardFeldman <eddie@eddie-pro.com>
2022-08-16 21:31:03 -0500

    Created Week7

--- Week5/Javascript/Chapter4/exercise.js
+++ Week5/Javascript/Chapter4/exercise.js
@@ -1,68 +1,72 @@
-// Chapter 3 
-//a.Write a function min that takes two arguments and returns their minimum
-const smallestArg = function(a,b) {
-  return a<b?a:b;
+// Chapter 4 
+//a.Write a function sum that takes other function with two arguments as range and returns their sum
+ const range = function(a,b,step) {
+  let arr=[a,b,step];
+  return arr;
 };
-console.log(smallestArg(-3,4));
 
-console.log(smallestArg(503,224));
 
-//b.Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number)
+const sum = function(a) {
+  let sum=0;
+  let s=(a[2]!=null)?a[2]:1;
+  for(let x=a[0];x<=a[1];x+=x*s)
+        sum=sum+x;
+  return sum;
+};
 
-//and returns string
-function isEven(e) {
-  if(e%1>0) 
-    return "error! input must be any whole number";
-   if (isNaN(e))
-    return "error! input must be any whole number";
-  if(e<0)
-    e= Math.abs(e);
-  if(e===1)
-    return "is odd";
-  if (e === 0) {
-    return "is even";
-  } else {
-    e=e-2;
-    return isEven(e)
+console.log(sum(range(1, 10,1)));
+
+//b.Define function that produces a new array that has the same elements in the inverse order
+
  
+function reverseArr(e) {
+     return e.reverse();
   }
+
+console.log( reverseArr([1,3,5]));
+console.log( reverseArr(['C','B','A']));
+ 
+
+
+//c. Write a function arrayToList that builds up a list structure like the one
+//shown when given [1, 2, 3] as argument. Also write a listToArray
+//function that produces an array from a list.
+
+const arrayToList = function(arr) {
+	let mylist={value:arr[i].[0],rest:arr[i].[1]};
+	for(let i=0;i<arr.length;i++)
+		mylist.Add(arr[i]);
+	return mylist;
+}
+const arrayToList = function(mylist) {
+	let arr=[];
+	let i=0;
+	for (const item in mylist)
+	{	
+	arr.indexOf(i)=item;
+	i++;
+	}
+	return arr;
 }
 
-console.log(isEven(50));
-// → true
-console.log(isEven(75));
-// → false
-console.log(isEven(-1));
-// → ??
-console.log(isEven(-12));
-console.log(isEven(-2.3));
-
-//c. Write a function countBs that takes a string as its only argument and returns 
-//a number that indicates how many uppercase “B” characters there are in the string.
-
-const countBs = function(mystring) {
-  let counter=0;
-  let i=0;
-  console.log(i);
-    console.log(mystring[i]);
-  for(let i=0;i<mystring.length;i++){
-    
-    if(mystring[i]=='B')
-      counter=counter+1;
-  }
-  return counter;
-};
-/* const countChar = function(mystring,a) {
-return countBs(s);
-  }
-  
-};
-console.log(countChar("kakkerlak", "k"));
-// → 4
-*/
-console.log(countBs("BbSSSbbBBSBC"));
-// → 2
-
+const listToArray = function(mylist) {
+	let arr=[];
+	let i=0;
+	for (let item in mylist)
+	{	
+     //  console.log(item.date);
+	arr[i]=item.value;
+	i++;
+	}
+	return arr;
+}
+ 
+let mylist=[
+    { date: '12/1/2011', reading: 3, id: 20055 },
+    { date: '13/1/2011', reading: 5, id: 20053 },
+    { date: '14/1/2011', reading: 6, id: 45652 }
+];
+console.log(listToArray(mylist));
 
 
 
